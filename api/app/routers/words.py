2b5766927a6c62581f31556c39d5d379690203ee
xDuckxz <a.plsy.ajjp@gmail.com>
2025-11-01 14:00:09 +0700

    vocab_api

--- api/app/routers/words.py
+++ api/app/routers/words.py
@@ -8,14 +8,27 @@ router = APIRouter()
 
 
 words = [
-    { "word": "Ephemeral", "definition": "Lasting for a very short time.", "difficulty": "Advanced" },
-    { "word": "Ubiquitous", "definition": "Present, appearing, or found everywhere.", "difficulty": "Intermediate" },
-    { "word": "Mellifluous", "definition": "(Of a voice or words) sweet or musical; pleasant to hear.", "difficulty": "Advanced" },
-    { "word": "Serendipity", "definition": "The occurrence and development of events by chance in a happy or beneficial way.", "difficulty": "Intermediate" },
-    { "word": "Happy", "definition": "Feeling or showing pleasure or contentment.", "difficulty": "Beginner" },
-    { "word": "Run", "definition": "Move at a speed faster than a walk, never having both or all the feet on the ground at the same time.", "difficulty": "Beginner" }
-]
+    {
+        "id": 12,
+        "word": 'Ephemeral',
+        "definition": 'Lasting for a very shory time',
+        "difficulty_level": 'Advanced'
+    },
+#     { "word": "Ephemeral", "definition": "Lasting for a very short time.", "difficulty": "Advanced" },
+#     { "word": "Ubiquitous", "definition": "Present, appearing, or found everywhere.", "difficulty": "Intermediate" },
+#     { "word": "Mellifluous", "definition": "(Of a voice or words) sweet or musical; pleasant to hear.", "difficulty": "Advanced" },
+#     { "word": "Serendipity", "definition": "The occurrence and development of events by chance in a happy or beneficial way.", "difficulty": "Intermediate" },
+#     { "word": "Happy", "definition": "Feeling or showing pleasure or contentment.", "difficulty": "Beginner" },
+#     { "word": "Run", "definition": "Move at a speed faster than a walk, never having both or all the feet on the ground at the same time.", "difficulty": "Beginner" }
+ ]
 
 @router.get("/word", response_model=WordResponse)
 def get_random_word():
-    ... 
+    random_word = random.choice(words)
+    return random_word
+
+@router.get('/healthcheck')
+def healthcheck():
+    return "healthcheck: OKAY"
+    
+    
--- api/main.py
+++ api/main.py
@@ -1,4 +1,6 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
+from app.schemas import WordResponse
+from app.routers import words
 
 # Initialize FastAPI app
 app = FastAPI(
@@ -7,15 +9,22 @@ app = FastAPI(
     description="API for vocabulary practice and learning"
 )
 
-@app.get("/api/word")
-def get_random_word():
-    """Get a random word"""
-    # TODO Write logic here....
-    return {
-        "word": "example",
-        "definition": "a representative form or pattern",
-        "difficulty_level": "Beginner"
-    }
+app.include_router(
+    words.router,
+    prefix='/api',
+    tags=["words"]
+)
+
+# @app.get("/api/word" , respornse_model=WordResponse)
+# def get_random_word():
+#     """Get a random word"""
+#     # TODO Write logic here....
+#     words = []
+#     if len(words) == 0:
+#         raise HTTPException(
+#             status_code = 404,
+#             detail='No words available in databases'
+#         )
 
 @app.get("/")
 def read_root():
